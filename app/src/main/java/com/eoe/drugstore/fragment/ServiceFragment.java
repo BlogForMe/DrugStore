9925065a3bdaad936d4a248fcda8643f418f16fa
BlogForMe <huaiyiheyuan@gmail.com>
2017-06-12 12:27:53 +0800

    优化

--- app/src/main/java/com/eoe/drugstore/fragment/ServiceFragment.java
+++ app/src/main/java/com/eoe/drugstore/fragment/ServiceFragment.java
@@ -10,11 +10,25 @@ import android.view.ViewGroup;
 
 import com.eoe.drugstore.R;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.annotations.NonNull;
+
+
 /**
  * Created by Administrator on 2016/3/2.
  */
 public class ServiceFragment extends Fragment {
     private static final String TAG = "RAndroidSamples";
+    Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+        @Override
+        public void subscribe(@NonNull FlowableEmitter<String> e) throws Exception {
+            e.onNext("hellow RxJava2");
+            e.onComplete();
+        }
+    }, BackpressureStrategy.BUFFER);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
