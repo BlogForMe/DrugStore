8f250678f3660ebe90a9c08d32564a0b1b685323
BlogForMe <huaiyiheyuan@gmail.com>
2017-07-05 18:41:04 +0800

    播放

--- app/src/main/java/com/eoe/drugstore/fragment/VideoFragment.java
+++ app/src/main/java/com/eoe/drugstore/fragment/VideoFragment.java
@@ -17,24 +17,18 @@ import com.eoe.drugstore.R;
 import com.eoe.drugstore.exoplayer2.EventLogger;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.dash.DashChunkSource;
-import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 import static com.eoe.drugstore.utils.Constants.WRITE_EXTERNAL_STORAGE_REQUEST_CODE;
@@ -46,10 +40,9 @@ import static com.eoe.drugstore.utils.Constants.WRITE_EXTERNAL_STORAGE_REQUEST_C
 public class VideoFragment extends BaseFragment {
     public static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
     private SimpleExoPlayer player;
-    //    String vUrl = Environment.getExternalStorageDirectory().getPath()  /*+"/mv.mp4"*/ + "/xmy.flv";
-    String vUrl = "http://107.173.10.164/Demo/evedio/XMYP37.flv";
+    //    String vUrl = "http://107.173.10.164/Demo/evedio/XMYP37.flv";
+    String vUrl = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
     private SimpleExoPlayerView simpleExoPlayerView;
-    //    Uri mp4VideoUri = Uri.parse("https://storage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%20Hangin'%20with%20the%20Google%20Search%20Bar.mp4");
     Uri mp4VideoUri = Uri.parse(vUrl);
     String extenstion;
     private DataSource.Factory mediaDataSourceFactory;
@@ -108,35 +101,23 @@ public class VideoFragment extends BaseFragment {
     }
 
 
-    DefaultTrackSelector trackSelector = new DefaultTrackSelector();
-
     private void initializePlayer() {
-        TrackSelection.Factory adaptiveTrackSectionFactory =
+        TrackSelection.Factory videoTrackSectionFactory =
                 new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
 
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSectionFactory);
+        LoadControl loadControl = new DefaultLoadControl();
+        DataSource.Factory dataSourceFactory = buildDataSourceFactory(true);
+
+
         //2.创建ExoPlayer
-        player = ExoPlayerFactory.newSimpleInstance(
-                new DefaultRenderersFactory(getActivity()),
-                new DefaultTrackSelector(adaptiveTrackSectionFactory),
-                new DefaultLoadControl());
-//        eventLogger = new EventLogger(trackSelector);
+        MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri,
+                dataSourceFactory, new DefaultExtractorsFactory(), null, null);
+
+        player = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+        eventLogger = new EventLogger(trackSelector);
         simpleExoPlayerView.setPlayer(player);
-        player.setPlayWhenReady(true);
-//        MediaSource mediaSource = new ExtractorMediaSource(mp4VideoUri, mediaDataSourceFactory, new DefaultExtractorsFactory(),
-//                mainHandler, eventLogger);
-        MediaSource mediaSource = buildMediaSource(mp4VideoUri);
-
-//        boolean haveResumePosition = resumePosition != C.INDEX_UNSET;
-//        if (haveResumePosition) {
-//            player.seekTo(resumeWindow, resumePosition);
-//        }
-        player.prepare(mediaSource, false, false);
-    }
-
-    private MediaSource buildMediaSource(Uri uri) {
-        DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER);
-        DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(dataSourceFactory);
-        return new DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null);
+        player.prepare(videoSource);
     }
 
 
@@ -156,6 +137,15 @@ public class VideoFragment extends BaseFragment {
         player.release();
     }
 
+
+    private void hideSystemUi() {
+        simpleExoPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
     private void permissionCheck() {
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
