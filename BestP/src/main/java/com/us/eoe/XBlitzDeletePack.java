5e313b1e34ac1898724d3e5967c7166390f8111c
Jon <huaiyiheyuan@gmail.com>
2017-07-26 11:26:46 +0800

    天气

---
+++ BestP/src/main/java/com/us/eoe/XBlitzDeletePack.java
@@ -0,0 +1,15 @@
+package com.us.eoe;
+
+import android.content.pm.IPackageDeleteObserver;
+import android.os.RemoteException;
+
+/**
+ * Created by blitzfeng on 2017/7/13.
+ */
+
+public class XBlitzDeletePack extends IPackageDeleteObserver.Stub {
+    @Override
+    public void packageDeleted(String packageName, int returnCode) throws RemoteException {
+        System.out.println("delete packageName:"+packageName+"---returnCode:"+returnCode);
+    }
+}
---
+++ BestP/src/main/java/com/us/eoe/utils/XposeUtil.java
@@ -0,0 +1,12 @@
+package com.us.eoe.utils;
+
+/**
+ * Created by blitzfeng on 2017/7/19.
+ */
+
+public class XposeUtil {
+
+    public static String pkg1 = "de.robv.android.xpose.installer";
+    public static String pkg2 = "com.blitz.ice.xadcheat";
+
+}
